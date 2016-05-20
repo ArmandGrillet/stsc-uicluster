ab404bf8cebb40104f702067bcaa67aaba35068e
Armand Grillet <ArmandGrillet@users.noreply.github.com>
2016-05-20 14:23:48 +0200

    UI improvements.

--- src/main/scala/uicluster/Controller.scala
+++ src/main/scala/uicluster/Controller.scala
@@ -1,26 +1,26 @@
 package uicluster
 
+// UI
+import java.awt.{Color, Graphics2D, Paint}
+import java.awt.image.BufferedImage
 import javafx.scene.{chart => jfxsc}
-import scalafx.scene.Node
-import scalafx.scene.control.TextField
-import scalafx.scene.control.Button
-import scalafx.event.ActionEvent
-import scalafxml.core.macros.sfxml
-import scalafxml.core.{NoDependencyResolver, FXMLLoader}
-import scalafx.scene.layout.AnchorPane
-import scalafx.scene.chart._
-import scalafx.scene.image.ImageView
-import scalafx.stage.FileChooser
-import scalafx.stage.FileChooser.ExtensionFilter
-import scalafx.scene.control.Alert
-import scalafx.scene.control.Alert._
-import scalafx.scene.control.Label
-
-import java.awt.{Color, Paint}
+import org.jfree.chart.axis.{NumberTickUnit, TickUnits}
 import scala.collection.mutable.ListBuffer
 import scala.util.control._
-import org.jfree.chart.axis.{NumberTickUnit, TickUnits}
+import scalafx.embed.swing.SwingFXUtils
+import scalafx.event.ActionEvent
+import scalafx.scene.chart._
+import scalafx.scene.control._
+import scalafx.scene.control.Alert._
+import scalafx.scene.image.ImageView
+import scalafx.scene.layout.AnchorPane
+import scalafx.scene.Node
+import scalafx.stage.FileChooser
+import scalafx.stage.FileChooser.ExtensionFilter
+import scalafxml.core.{NoDependencyResolver, FXMLLoader}
+import scalafxml.core.macros.sfxml
 
+// Logic
 import breeze.linalg._
 import breeze.numerics._
 import breeze.stats._
@@ -28,14 +28,17 @@ import breeze.plot._
 import java.io.File
 import stsc._
 
-import java.awt.image.BufferedImage
-import java.awt.Graphics2D
-import scalafx.embed.swing.SwingFXUtils
-
 @sfxml
-class Controller(private val root: AnchorPane, private val min: TextField, private val max: TextField, private val dataset: ImageView, private val clusters: ImageView, private val qualities: Label) {
-
+class Controller(private val root: AnchorPane, private val selectDataset: ChoiceBox[String], private val min: TextField, private val max: TextField, private val dataset: ImageView, private val clusters: ImageView, private val qualities: Label) {
     private var displayedDataset = DenseMatrix.zeros[Double](0, 0)
+
+    selectDataset.value.onChange {
+        if (selectDataset.value.value.takeRight(4) == ".csv") {
+            val dataset = new File(getClass.getResource("/" + selectDataset.value.value).getPath())
+            displayedDataset = breeze.linalg.csvread(dataset)
+            showDataset()
+        }
+    }
 
     def cluster(event: ActionEvent) {
         var minClusters = toInt(min.text.value).getOrElse(0)
@@ -58,7 +61,7 @@ class Controller(private val root: AnchorPane, private val min: TextField, priva
         }
 
         if (displayedDataset.rows == 0) {
-            showAlert("Needs a dataset", "TSelect a dataset before clustering it.")
+            showAlert("Needs a dataset", "Select a dataset before clustering it.")
             ready = false
         }
 
@@ -79,7 +82,7 @@ class Controller(private val root: AnchorPane, private val min: TextField, priva
         }
     }
 
-    def selectDataset(event: ActionEvent) {
+    def loadDataset(event: ActionEvent) {
         val fileChooser = new FileChooser {
             title = "Choose dataset"
             extensionFilters.add(new ExtensionFilter("CSV Files", "*.csv"))
@@ -90,18 +93,19 @@ class Controller(private val root: AnchorPane, private val min: TextField, priva
             if (dataset.cols > 2) {
                 showAlert("Too many dimensions", "There are " + dataset.cols + "in your CSV, we only need 2.")
             } else {
+                selectDataset.value = "Select dataset"
                 displayedDataset = dataset
                 showDataset()
             }
         }
     }
 
-    private def toInt(s: String): Option[Int] = {
-        try {
-            Some(s.toInt)
-        } catch {
-            case e: Exception => None
-        }
+    private def imageToFigure(f: Figure): BufferedImage = {
+        val image = new BufferedImage(f.width, f.height, BufferedImage.TYPE_INT_ARGB)
+        val g2d = image.createGraphics()
+        f.drawPlots(g2d)
+        g2d.dispose
+        return image
     }
 
     private def showAlert(header: String, content: String) {
@@ -124,11 +128,11 @@ class Controller(private val root: AnchorPane, private val min: TextField, priva
         qualities.text = ""
     }
 
-    private def imageToFigure(f: Figure): BufferedImage = {
-        val image = new BufferedImage(f.width, f.height, BufferedImage.TYPE_INT_ARGB)
-        val g2d = image.createGraphics()
-        f.drawPlots(g2d)
-        g2d.dispose
-        return image
+    private def toInt(s: String): Option[Int] = {
+        try {
+            Some(s.toInt)
+        } catch {
+            case e: Exception => None
+        }
     }
 }
--- src/main/scala/uicluster/View.scala
+++ src/main/scala/uicluster/View.scala
@@ -1,15 +1,13 @@
 package uicluster
 
-import java.io.IOException
-import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.Includes._
 import scalafx.scene.Scene
 import scalafxml.core.{NoDependencyResolver, FXMLView}
 
 object View extends JFXApp {
     val view = FXMLView(getClass.getResource("/uicluster.fxml"), NoDependencyResolver)
-
     stage = new PrimaryStage() {
         title = "Self-Tuning Spectral Clustering"
         resizable = false
